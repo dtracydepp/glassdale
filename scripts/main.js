1a7644babe857467d4f086564820c24f18a442ec
dtracydepp <tracydepp82@gmail.com>
2020-10-19 12:05:42 -0500

    created the criminal component

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
+import { Criminal } from "./Criminal.js"
 import { getCriminals } from "./CriminalDataProvider.js"
 import { getOfficers } from "./OfficerProvider.js"
 
 getOfficers()
 getCriminals()
+Criminal()
